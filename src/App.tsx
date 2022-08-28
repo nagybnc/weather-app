273f63bc5374f4afce9c06b878ce64dc9c3fb8ba
Gergely Bence Nagy <gergely_bence_nagy@epam.com>
2022-08-28 08:58:44 +0200

    Add wrapper styles and some CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ErrorBoundary } from "react-error-boundary";
 import { Error, ErrorHandler } from "./components/Error";
 import { useUserSettings } from "./hooks/useUserSettings";
 import { useWeather } from "./hooks/useWeather";
+import { ThemeType } from "./utils/store";
 
 function App() {
     const { userSettings, changeUserSettings } = useUserSettings();
@@ -17,11 +18,21 @@ function App() {
     console.groupEnd();
 
     return (
-        <h1 className="text-3xl bg-red-500 text-center">
-            <ErrorBoundary FallbackComponent={Error} onError={ErrorHandler}>
-                Weather app init
-            </ErrorBoundary>
-        </h1>
+        <div id="wrapper" className={`${userSettings.theme === ThemeType.light ? "theme-light" : "theme-dark"} content-wrapper`}>
+            <div className={`shadow-gray-400 mx-auto h-fit max-w-screen-lg bg-background-primary py-5 px-4 shadow-xl sm:my-4`}>
+                <ErrorBoundary FallbackComponent={Error} onError={ErrorHandler}>
+                    {isLoading ? (
+                        <div className="flex w-full flex-col items-center justify-center overflow-hidden opacity-75">
+                            <div className="loader mb-4 h-12 w-12 rounded-full border-4 border-t-4 border-colors-primary border-t-colors-secondary ease-linear"></div>
+                            <h2 className="text-center text-xl font-semibold text-colors-primary">Loading...</h2>
+                            <p className="w-2/3 text-center text-colors-primary">Loading description text....</p>
+                        </div>
+                    ) : (
+                        <div className="text-colors-primary">TODO</div>
+                    )}
+                </ErrorBoundary>
+            </div>
+        </div>
     );
 }
 
